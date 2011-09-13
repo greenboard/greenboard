08182b8f49af5090c2981e02255c3163ce76a1bf
Adrian <adrian.pillinger@gmail.com>
2011-09-13 21:53:01 +0100

    Fixing config so no force.com config is setup and the app starts up in tomcat

--- src/main/java/org/greenboard/controller/HomeController.java
+++ src/main/java/org/greenboard/controller/HomeController.java
@@ -1,14 +1,10 @@
 package org.greenboard.controller;
 
-import javax.inject.Inject;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import org.greenboard.service.EntityService;
 
 /**
  * Handles requests for the application home page.
@@ -16,19 +12,16 @@ import org.greenboard.service.EntityService;
 @Controller
 public class HomeController {
 
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	private static final Logger logger = LoggerFactory
+			.getLogger(HomeController.class);
 
-	@Inject
-	EntityService entityService;
-	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value="/", method=RequestMethod.GET)
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
-	    	logger.info("requesting home");
+		logger.info("requesting home");
 		return "home";
 	}
 
 }
-
