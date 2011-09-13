7961846ac757cb7594ceacfce81fac4c66bd3b7c
anumonk <anu.monk01@gmail.com>
2011-09-13 22:34:16 +0100

    Cleaned up Board entity #6

--- src/main/java/com/greenboard/board/Board.java
+++ src/main/java/com/greenboard/board/Board.java
@@ -2,6 +2,8 @@ package com.greenboard.board;
 
 import javax.persistence.Entity;
 
+import org.springframework.util.Assert;
+
 /**
  * Represents the central entity within Greenboard. Boards must have a spatial
  * location, together with an administrator who can audit the board.
@@ -15,4 +17,23 @@ import javax.persistence.Entity;
 @Entity
 public class Board {
 
+	private String name;
+	private String description;
+
+	public Board(String name) {
+		Assert.notNull(name, "name cannot be null");
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
