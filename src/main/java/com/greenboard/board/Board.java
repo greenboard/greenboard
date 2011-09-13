0ebe5b2480c8cfd381dfdf5a52104dcdaea3c0d1
anumonk <anu.monk01@gmail.com>
2011-09-13 22:28:18 +0100

    Started to add Board entity #6

---
+++ src/main/java/com/greenboard/board/Board.java
@@ -0,0 +1,18 @@
+package com.greenboard.board;
+
+import javax.persistence.Entity;
+
+/**
+ * Represents the central entity within Greenboard. Boards must have a spatial
+ * location, together with an administrator who can audit the board.
+ * 
+ * TODO Elaborate domain model around Boards (Private/Public boards), Cards and
+ * Pins.
+ * 
+ * @author Anu
+ * 
+ */
+@Entity
+public class Board {
+
+}
